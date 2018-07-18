ebda73b3534ea4388a2c5ed4e1ea108808a9dfe1
louyang <louyang367@gmail.com>
2018-07-18 13:21:00 -0700

    wiki body should be datatype text

--- src/db/models/wiki.js
+++ src/db/models/wiki.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     body: {
-      type: DataTypes.STRING
+      type: DataTypes.TEXT
     },
     private: {
       type: DataTypes.BOOLEAN,
